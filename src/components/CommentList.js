75df616b59ec814b56660fc3ab95a42db1301321
Reto Kaiser <reto@cargomedia.ch>
2016-10-19 18:52:26 +0200

    commit

---
+++ src/components/CommentList.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import Comment from './Comment'
+
+var CommentList = React.createClass({
+  propTypes: {
+    commentList: React.PropTypes.instanceOf(Array).isRequired,
+  },
+  render: function() {
+    return (
+      <div className="commentList">
+        {this.props.commentList.map(comment =>
+          <Comment author={comment.author} key={comment.id}>
+            {comment.text}
+          </Comment>
+        )}
+      </div>
+    );
+  }
+});
+
+export default CommentList;
